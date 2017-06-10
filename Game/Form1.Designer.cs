303c0ba86940dd7e2c11c3bdb7c6ca26175118f3
Prathik <prathikyr@gmail.com>
2017-06-10 19:35:54 +0530

    Running

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(96, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(152, 49);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(208, 49);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(264, 49);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(96, 105);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(152, 105);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(208, 105);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(264, 105);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(96, 161);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(152, 161);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(208, 161);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(264, 161);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(96, 217);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox13.TabIndex = 12;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(152, 217);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox14.TabIndex = 13;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(208, 217);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox15.TabIndex = 14;
+            this.pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Location = new System.Drawing.Point(264, 217);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox16.TabIndex = 15;
+            this.pictureBox16.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 313);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox16;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            KeyDown += new KeyEventHandler(move);
+            start_program();
+        }
+        int gameNotOver = 1;
+
+         int[,] board = new int[,]
+          {
+               {0, 0, 0, 0},
+               {0, 0, 0, 0},
+               {0, 0, 0, 0},
+               {0, 0, 0, 0}
+          };
+
+         int[,] prevState = new int[,]
+        {
+               {0, 0, 0, 0},
+               {0, 0, 0, 0},
+               {0, 0, 0, 0},
+               {0, 0, 0, 0}
+        };
+
+         public void initBoard()
+        {
+
+            int pick1, pick2;
+            Random rnd = new Random();
+            do
+            {
+                pick1 = rnd.Next(1, 17) - 1;
+                pick2 = rnd.Next(1, 17) - 1;
+            } while (pick1 == pick2);
+            //Console.WriteLine(pick1);
+            //Console.WriteLine(pick2);
+            board[pick1 / 4, (pick1) % 4] = 2;
+            board[pick2 / 4, (pick2) % 4] = 2;
+
+        }
+
+
+         public void displayBoard()
+        {
+            //for (int i = 0; i < 4; i++)
+            //{
+              //  for (int j = 0; j < 4; j++)
+                //{
+                    //Console.Write(board[i, j] + "\t");
+            pictureBox1.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[0,0].ToString() + ".jpg";
+            pictureBox2.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[0,1].ToString() + ".jpg";
+            pictureBox3.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[0,2].ToString() + ".jpg";
+            pictureBox4.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[0,3].ToString() + ".jpg";
+            pictureBox5.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[1,0].ToString() + ".jpg";
+            pictureBox6.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[1,1].ToString() + ".jpg";
+            pictureBox7.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[1,2].ToString() + ".jpg";
+            pictureBox8.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[1,3].ToString() + ".jpg";
+            pictureBox9.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[2,0].ToString() + ".jpg";
+            pictureBox10.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[2,1].ToString() + ".jpg";
+            pictureBox11.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[2,2].ToString() + ".jpg";
+            pictureBox12.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[2,3].ToString() + ".jpg";
+            pictureBox13.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[3,0].ToString() + ".jpg";
+            pictureBox14.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[3,1].ToString() + ".jpg";
+            pictureBox15.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[3,2].ToString() + ".jpg";
+            pictureBox16.ImageLocation = @"C:\Users\ANEESH\Desktop\image" + board[3,3].ToString() + ".jpg";
+
+           
+
+            //}
+            //Console.WriteLine();
+            //}
+
+            //Image image = Image.FromFile(@"C:\Users\ANEESH\Desktop\image2.jpg");
+            //pictureBox1.Image = image;
+
+        }
+
+         void genereateNewItem()
+        {
+            Random rnd = new Random();
+            int pick, row, col;
+            do
+            {
+                pick = rnd.Next(1, 17) - 1;
+                row = pick / 4;
+                col = pick % 4;
+            } while (board[row, col] != 0);
+            board[row, col] = 2;
+        }
+
+         int changeInState()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (board[i, j] != prevState[i, j])
+                        return 1;
+            return 0;
+        }
+
+         void copyState()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    prevState[i, j] = board[i, j];
+        }
+
+
+         int filledAll()
+        {
+            int filled = 1;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (board[i, j] == 0)
+                        filled = 0;
+            return filled;
+        }
+
+         int checkGameOver()
+        {
+            return filledAll();// && CheckCollapseDown() && CheckCollapseUp() && CheckCollapseLeft() && CheckCollapseRight();
+        }
+
+         public void moveDown()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //Code here for ith column to collapse
+                int[] temp = new int[4];
+                int j, k = 0;
+                for (j = 0; j < 4; j++)
+                {
+                    if (board[j, i] != 0)
+                    {
+                        temp[k] = board[j, i];
+                        k++;
+                    }
+                }
+                //total = k;
+                k--;
+                for (j = 3; j >= 0 && k >= 0; j--)
+                {
+                    board[j, i] = temp[k];
+                    k--;
+                }
+
+                while (j >= 0)
+                {
+                    board[j, i] = 0;
+                    j--;
+                }
+
+                if ((board[3, i] == board[2, i]) && (board[3, i] != 0))
+                {
+                    board[3, i] = board[2, i] + board[3, i];
+                    board[2, i] = board[1, i];
+                    board[1, i] = board[0, i];
+                    board[0, i] = 0;
+                }
+                if ((board[2, i] == board[1, i]) && (board[2, i] != 0))
+                {
+                    board[2, i] = board[1, i] + board[2, i];
+                    board[1, i] = board[0, i];
+                    board[0, i] = 0;
+                }
+                if ((board[1, i] == board[0, i]) && (board[1, i] != 0))
+                {
+                    board[1, i] = board[0, i] + board[1, i];
+                    board[0, i] = 0;
+                }
+            }
+        }
+
+
+         public void moveUp()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //Code here for ith column to collapse
+                int[] temp = new int[4];
+                int j, k = 0;
+                for (j = 0; j < 4; j++)
+                {
+                    if (board[j, i] != 0)
+                    {
+                        temp[k] = board[j, i];
+                        k++;
+                    }
+                }
+                int total = k;
+                k = 0;
+                for (j = 0; j <= 3 && k < total; j++)
+                {
+                    board[j, i] = temp[k];
+                    k++;
+                }
+
+                while (j <= 3)
+                {
+                    board[j, i] = 0;
+                    j++;
+                }
+
+                if ((board[0, i] == board[1, i]) && (board[0, i] != 0))
+                {
+                    board[0, i] = board[0, i] + board[1, i];
+                    board[1, i] = board[2, i];
+                    board[2, i] = board[3, i];
+                    board[3, i] = 0;
+                }
+                if ((board[1, i] == board[2, i]) && (board[1, i] != 0))
+                {
+                    board[1, i] = board[1, i] + board[2, i];
+                    board[2, i] = board[3, i];
+                    board[3, i] = 0;
+                }
+                if ((board[2, i] == board[3, i]) && (board[2, i] != 0))
+                {
+                    board[2, i] = board[2, i] + board[3, i];
+                    board[3, i] = 0;
+                }
+            }
+        }
+
+        public void moveLeft()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //Code here for ith row to collapse
+                int[] temp = new int[4];
+                int j, k = 0;
+                for (j = 0; j < 4; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        temp[k] = board[i, j];
+                        k++;
+                    }
+                }
+                int total = k;
+                k = 0;
+                for (j = 0; j <= 3 && k < total; j++)
+                {
+                    board[i, j] = temp[k];
+                    k++;
+                }
+
+                while (j <= 3)
+                {
+                    board[i, j] = 0;
+                    j++;
+                }
+
+                if ((board[i, 0] == board[i, 1]) && (board[i, 0] != 0))
+                {
+                    board[i, 0] = board[i, 0] + board[i, 1];
+                    board[i, 1] = board[i, 2];
+                    board[i, 2] = board[i, 3];
+                    board[i, 3] = 0;
+                }
+                if ((board[i, 1] == board[i, 2]) && (board[i, 2] != 0))
+                {
+                    board[i, 1] = board[i, 1] + board[i, 2];
+                    board[i, 2] = board[i, 3];
+                    board[i, 3] = 0;
+                }
+                if ((board[i, 2] == board[i, 3]) && (board[i, 2] != 0))
+                {
+                    board[i, 2] = board[i, 2] + board[i, 3];
+                    board[i, 3] = 0;
+                }
+            }
+        }
+
+        public void moveRight()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //Code here for ith row to collapse
+                int[] temp = new int[4];
+                int j, k = 0;
+                for (j = 0; j < 4; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        temp[k] = board[i, j];
+                        k++;
+                    }
+                }
+                //total = k;
+                k--;
+                for (j = 3; j >= 0 && k >= 0; j--)
+                {
+                    board[i, j] = temp[k];
+                    k--;
+                }
+
+                while (j >= 0)
+                {
+                    board[i, j] = 0;
+                    j--;
+                }
+
+                if ((board[i, 3] == board[i, 2]) && (board[i, 3] != 0))
+                {
+                    board[i, 3] = board[i, 2] + board[i, 3];
+                    board[i, 2] = board[i, 1];
+                    board[i, 1] = board[i, 0];
+                    board[i, 0] = 0;
+                }
+                if ((board[i, 2] == board[i, 1]) && (board[i, 2] != 0))
+                {
+                    board[i, 2] = board[i, 1] + board[i, 2];
+                    board[i, 1] = board[i, 0];
+                    board[i, 0] = 0;
+                }
+                if ((board[i, 1] == board[i, 0]) && (board[i, 1] != 0))
+                {
+                    board[i, 1] = board[i, 0] + board[i, 1];
+                    board[i, 0] = 0;
+                }
+            }
+        }
+
+        public void move(object sender, KeyEventArgs input)
+        {
+            //Console.WriteLine("key was pressed");
+            copyState();
+            if (input.KeyCode == Keys.Down)
+            {
+                //CheckCollapseDown();
+                moveDown();
+                //return 1;
+            }
+            else if (input.KeyCode == Keys.Up)
+            {
+                //CheckCollapseUp();
+                moveUp();
+                //return 1;
+            }
+            else if (input.KeyCode == Keys.Left)
+            {
+                //CheckCollapseLeft();
+                moveLeft();
+                //return 1;
+            }
+            else if (input.KeyCode == Keys.Right)
+            {
+                //CheckCollapseRight();
+                moveRight();
+                //return 1;
+            }
+            //else
+            //  ;// return 0;
+            genereateNewItem();
+            displayBoard();
+
+            gameNotOver = checkGameOver();
+
+        }
+        void start_program()
+        {
+            initBoard();
+            //Console.WriteLine("Hello World");
+            //Console.Clear();
+            displayBoard();
+            /*int gameNotOver = 1;
+            while (gameNotOver == 1)
+            {
+
+                //Console.WriteLine(key);
+                do
+                {
+                    //ConsoleKeyInfo input = Console.ReadKey(true);
+                    //gameNotOver = move(input);
+                } while (changeInState() == 0);
+
+                genereateNewItem();
+
+                gameNotOver = checkGameOver();
+                if(gameNotOver == 0)
+                	break;
+
+                //Console.WriteLine("-----------------");
+                //Console.Clear();*/
+                //displayBoard();
+
+           // }
+            
+        }
+        
+    }
+}
